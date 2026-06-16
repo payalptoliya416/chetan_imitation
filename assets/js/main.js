dde89b36b9054465817ae15af640cc0640dfc4e4
payalptoliya416 <payal.rising416@gmail.com>
2026-06-16 17:59:24 +0530

    code

--- assets/js/main.js
+++ assets/js/main.js
@@ -565,3 +565,30 @@ if (overlay && sidebar) {
     }
 
 }
+
+// -----------OTP input
+
+const inputs = document.querySelectorAll('.otp-input');
+
+inputs.forEach((input, index) => {
+
+    input.addEventListener('input', (e) => {
+
+        // માત્ર 1 digit રાખવી
+        e.target.value = e.target.value.replace(/[^0-9]/g, '');
+
+        // Next input પર જવું
+        if (e.target.value && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+
+        // Backspace માટે
+        if (e.key === 'Backspace' && !input.value && index > 0) {
+            inputs[index - 1].focus();
+        }
+    });
+
+});
